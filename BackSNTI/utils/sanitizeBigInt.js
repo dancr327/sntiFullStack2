5c96a42b918b70b1b18bb168ed2caeef6a47223a
dancr327 <cardosa.mx@gmail.com>
2025-07-02 19:07:06 -0600

    commit de martos bbb0f06 ya funciona el backend, falta acoplarlo al back

---
+++ BackSNTI/utils/sanitizeBigInt.js
@@ -0,0 +1,15 @@
+function sanitizeBigInt(obj) {
+  if (Array.isArray(obj)) return obj.map(sanitizeBigInt);
+  else if (obj && typeof obj === 'object') {
+    const result = {};
+    for (const key in obj) {
+      const value = obj[key];
+      if (typeof value === 'bigint') result[key] = value.toString();
+      else if (value instanceof Date) result[key] = value.toISOString(); // "2025-06-21T00:00:00.000Z"
+      else result[key] = sanitizeBigInt(value);
+    }
+    return result;
+  }
+  return obj;
+}
+module.exports = sanitizeBigInt;
